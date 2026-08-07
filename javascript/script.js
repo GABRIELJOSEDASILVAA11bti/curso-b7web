95cd95352781554b0f7d0c51b64c2345bbe1a23f
GABRIELJOSEDASILVAA11bti <gj6987917@gmail.com>
2026-08-07 20:57:59 -0300

    priemeiro commit

---
+++ javascript/script.js
@@ -0,0 +1,62 @@
+alert("Olá,Mundo!") // serve para aparecer uma msg na tela principal do usuario
+
+console.log("Olá,Mundo!")// aparece apenas no console(que está no botão inspecionar),serveria pra verificar um tipo de conta por exemplo.
+
+
+//VARIAVEIS
+
+// O (let) serve para criar uma variavel,tanto nome quanto numero. Oque muda é as aspas.
+
+let nome = "Victor Ikenny"
+let idade = 15
+let hobby = "Jogar bola"
+
+console.log(nome)
+console.log(idade)
+console.log(hobby)
+
+//Tipos de dados JavaScript
+
+//boolean: valores lógicos que podem ser apenas verdadeiro (true) ou falso (false).
+
+let idade15 = false
+let idade16 = true
+
+//String: textos escritos entre aspas, como "olá".
+
+let introdução = "é um prazer estar aprendendo javascript"
+
+// Number: números, sejam inteiros ou com pontos decimais, como 10 ou 3.14.
+
+let quantidade = 18
+let valor = 15.88
+
+// Array: uma estrutura de dados que armazena uma coleção de elementos.
+
+let listadcompra = ["maça", "banana", "laranja", "feijão", "arroz"]
+
+// Object: armazena dados nomeados através de propriedades e métodos.
+
+let usuario = { nome: "Ana", idade: 30 };
+
+//exercicios
+
+//exercicio-1 :  Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba-a.
+
+let carro = "ferrari"
+console.log(carro)
+//esqueci de colocar console.log e chamar a variavel.
+//Ficaria:  console.log(carro)
+
+// 2. Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba-a.
+
+let bolo_preco = 19.99
+console.log("bolo_preco é igual a ", bolo_preco ) //Aqui me embolei todo, confundi com python.
+
+// 3. Corrija o código a seguir (e exiba-o). let "cidade" = São Paulo
+
+let cidade = "Sao Paulo"
+console.log(cidade)
+ // esqueci de colocar console.log e de chamar a variavel.Se tiver no codigo, é pq ajustei.
+
+
