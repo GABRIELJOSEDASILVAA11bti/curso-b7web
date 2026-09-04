7245276bc011a8c1c725793919e0b9bf23c878d9
GABRIELJOSEDASILVAA11bti <gj6987917@gmail.com>
2026-09-04 20:04:28 -0300

    aula sobe Strings e Templates de Strings

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+//Exercício 1: Gerador de E-mail de Boas-VindasO que fazer: Crie três variáveis: nomeCliente, cursoComprado e linkAcesso. Use uma Template String para criar uma mensagem com múltiplas linhas (pressione Enter dentro das crases para quebrar a linha naturalmente no código). -- EXERCICIO SOBRE :Strings e Tamplate String
+
 alert("Olá,Mundo!") // serve para aparecer uma msg na tela principal do usuario
 
 console.log("Olá,Mundo!")// aparece apenas no console(que está no botão inspecionar),serveria pra verificar um tipo de conta por exemplo.
@@ -68,3 +70,29 @@ let aumento = 300;
 let novoSalario = salarioAtual + aumento
 
 console.log("Valore do novo salário é :",novoSalario)
+
+//Exercício 1: Gerador de E-mail de Boas-VindasO que fazer: Crie três variáveis: nomeCliente, cursoComprado e linkAcesso. Use uma Template String para criar uma mensagem com múltiplas linhas (pressione Enter dentro das crases para quebrar a linha naturalmente no código). -- EXERCICIO SOBRE :Strings e Tamplate String
+
+let nomeCliente = "Gabriel";
+let cursoComprado = "Programação web com gjota"
+let linkAcesso = "https://programaçãoweb.com"
+
+
+
+console.log(`Olá, ${nomeCliente}, !
+Seu acesso ao curso, ${cursoComprado}, já está liberado.
+Clique aqui para acessar:, ${linkAcesso}`)
+
+
+
+
+//  Exercício 2: Criador de Tag HTML (Falso "Front-end")O que fazer: Simule a criação de um componente para um site. Crie as variáveis tituloArtigo, autor e data. Use uma Template String para montar uma estrutura de texto simulando código HTML (usando as tags <h1> e <span>).Resultado esperado no console: "<h1>[tituloArtigo]</h1><span>Por: [autor] em [data]</span>"
+
+
+
+
+let textoQualquer = "A arte de aprender";
+let autor = "gjota briel";
+let data = new Date(2026, 8, 4);
+
+console.log(`<h1>, ${textoQualquer} </h1>, <span>, Por:, ${autor} em, ${data}, </span>`)
